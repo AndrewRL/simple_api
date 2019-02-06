224dbec9f091c1b2888115ae701117189c515b16
Andrew Laird <arlaird@gmail.com>
2019-02-06 15:21:17 -0800

    Added get_positions to api

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello, world!"
+def read_csv(path, skiplines=True, header=True):
+    data = []
+    with open(path, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        if header:
+            next(reader, None)
+        for row in reader:
+            data.append(row)
+    return data
+
+@app.route('/api/v1/positions', methods=['GET'])
+def get_positions():
+    return jsonify(read_csv('/home/ec2-user/test_positions.csv'))
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
