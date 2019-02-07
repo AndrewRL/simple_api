685eee89294702dee5d4c1fa56d86eb099dd672a
Andrew Laird <arlaird@gmail.com>
2019-02-06 16:46:05 -0800

    Fixed minor issues

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def read_csv(path, skiplines=True, header=True):
 def write_pos_file(pos_data, outfile):
     with open(outfile, 'a', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
-        csvwriter.writerow([row for row in pos_data])
+        csvwriter.writerows([row for row in pos_data])
 
 @app.route('/api/v1/positions', methods=['GET'])
 def get_positions():
@@ -27,13 +27,11 @@ def add_positions():
     if not request.json or not 'positions' in request.json:
         abort(400)
     
-    # load positions from csv
     all_positions = read_csv('/home/ec2-user/test_positions.csv')
     # create a list of position data from post + test_positions.csv
     all_positions.extend(request.json['positions'])
     # sort data by timestamp
     all_positions = sorted(all_positions, key=lambda x: float(x[1]))
-    print(all_positions)
     # save data to positions.csv
     write_pos_file(all_positions, 'positions.csv')
     return jsonify({'success': True}), 201
